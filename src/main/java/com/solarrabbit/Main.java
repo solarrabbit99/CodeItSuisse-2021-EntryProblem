a5c275031b61dc9377921ae779eb772b7e7a1f74
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-09-17 21:54:59 +0800

    Add more sample input and output files

--- src/main/java/com/solarrabbit/Main.java
+++ src/main/java/com/solarrabbit/Main.java
@@ -5,8 +5,8 @@ import org.json.simple.JSONObject;
 import com.solarrabbit.util.Storage;
 
 public class Main {
-    private static final String input = "./data/input.json";
-    private static final String output = "./data/output.json";
+    private static final String input = "./data/sampleInput4.json";
+    private static final String output = "./data/sampleOutput4.json";
 
     public static void main(String[] args) {
         Storage storage = new Storage(input, output);
