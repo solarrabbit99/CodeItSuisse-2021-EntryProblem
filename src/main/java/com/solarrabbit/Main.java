e308b1965492e412d5ba8de74d97bba6f0b39f36
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-09-18 16:43:32 +0800

    Add javadocs to Main

--- src/main/java/com/solarrabbit/Main.java
+++ src/main/java/com/solarrabbit/Main.java
@@ -4,9 +4,20 @@ import com.solarrabbit.file.Storage;
 
 import org.json.simple.JSONObject;
 
+/**
+ * Driver class.
+ */
 public class Main {
+    /**
+     * The output file name.
+     */
     private static final String output = "output.json";
 
+    /**
+     * Driver method.
+     *
+     * @param args input file name.
+     */
     public static void main(String[] args) {
         Storage storage = new Storage(args[0], output);
         JSONObject input = storage.load();
